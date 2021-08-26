c1da01fb30c000736b2982cff1362b2de88fa3d4
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2021-08-26 15:51:37 +0530

    Time: 156 ms (26.73%), Space: 14.9 MB (93.69%) - LeetHub

---
+++ interval-list-intersections/interval-list-intersections.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intervalIntersection(self, f: List[List[int]], s: List[List[int]]) -> List[List[int]]:
+        i=0
+        j=0
+        ans=[]
+        while(i<len(f) and j<len(s)):
+            if ((s[j][0]<=f[i][0] and f[i][0]<=s[j][1]) or (s[j][0]<=f[i][1] and f[i][1]<=s[j][1]) or (s[j][0]>=f[i][0] and f[i][1]>=s[j][0])):
+                ans.append([max(s[j][0],f[i][0]),min(f[i][1],s[j][1])])
+                if(s[j][1]<f[i][1]):
+                    j+=1
+                else:
+                    i+=1
+            else:
+                if(s[j][1]<f[i][1]):
+                    j+=1
+                else:
+                    i+=1
+        return ans
