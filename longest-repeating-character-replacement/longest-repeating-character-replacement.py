8dd5c423f063455b4813d0af674e85b7ffc35774
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2021-09-18 20:44:23 +0530

    Time: 1686 ms (5.01%), Space: 14.1 MB (99.05%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -2,30 +2,26 @@ from collections import Counter
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l=0
-        r=k
+        r=0
         ans=0
-        d=Counter(s[:k])
+        d={}
+        for i in range(65,91):
+            d[chr(i)]=0
         while(r<len(s)):
-            if(len(d)):
-                a=d.most_common(1)[0][1]
-                if(k>=r-l-a):
-                    if(s[r] in d):
-                        d[s[r]]+=1
-                    else:
-                        d[s[r]]=1
-                    ans=max(ans,r-l)
-                    r+=1
-                else:
-                    d[s[l]]-=1
-                    l+=1
-            else:
-                if(s[r] in d):
-                    d[s[r]]+=1
-                else:
-                    d[s[r]]=1
+            a=0
+            for i in range(65,91):
+                a=max(d[chr(i)],a)
+            # print(l,r,a)
+            if(k>=r-l-a):
+                d[s[r]]+=1
                 ans=max(ans,r-l)
                 r+=1
-        a=d.most_common(1)[0][1]
+            else:
+                d[s[l]]-=1
+                l+=1
+        a=0
+        for i in range(65,91):
+            a=max(d[chr(i)],a)
         if(k>=r-l-a):
             ans=max(ans,r-l)
         return ans
