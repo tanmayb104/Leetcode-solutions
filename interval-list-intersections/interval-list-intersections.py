9dfe8aece034b9242f8345d480ffa15b32740993
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2021-09-24 22:47:00 +0530

    Time: 152 ms (56.80%), Space: 15.2 MB (37.92%) - LeetHub

--- interval-list-intersections/interval-list-intersections.py
+++ interval-list-intersections/interval-list-intersections.py
@@ -4,15 +4,10 @@ class Solution:
         j=0
         ans=[]
         while(i<len(f) and j<len(s)):
-            if ((f[i][0]<=s[j][1]) and (s[j][0]<=f[i][1])):
+            if ((s[j][0]<=f[i][0] and f[i][0]<=s[j][1]) or (s[j][0]<=f[i][1] and f[i][1]<=s[j][1]) or (s[j][0]>=f[i][0] and f[i][1]>=s[j][0])):
                 ans.append([max(s[j][0],f[i][0]),min(f[i][1],s[j][1])])
-                if(s[j][1]<f[i][1]):
-                    j+=1
-                else:
-                    i+=1
+            if(s[j][1]<f[i][1]):
+                j+=1
             else:
-                if(s[j][1]<f[i][1]):
-                    j+=1
-                else:
-                    i+=1
+                i+=1
         return ans
