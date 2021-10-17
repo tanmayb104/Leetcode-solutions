313f6e2c081a7db577c86ca0bf24454bbd69c7f1
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2021-10-17 21:44:09 +0530

    Time: 360 ms (71.07%), Space: 21.9 MB (56.45%) - LeetHub

--- find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,22 +1,14 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        s=set()
+        s=[]
         for i in range(len(nums)):
-            if(nums[i]-1>=0):
-                # print(nums[i]-1)
-                if(nums[nums[i]-1]<0):
-                    s.add(nums[i])
-                else:
-                    nums[nums[i]-1]-=9999999
+            # print(nums)
+            if(nums[abs(nums[i])-1]<0):
+                s.append(abs(nums[i]))
             else:
-                a=nums[i]-1+9999999
-                # print(a)
-                if(nums[a]<0):
-                    s.add(a+1)
-                else:
-                    nums[a]-=9999999
+                nums[abs(nums[i])-1]*=-1
                 
-        return list(s)
+        return s
     
 # 2 1 3 5 4
 # 2 -1 3 5 4
