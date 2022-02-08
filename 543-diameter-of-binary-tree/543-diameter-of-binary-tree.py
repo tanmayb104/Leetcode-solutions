2e8ca9572b5bc4f759285ad00c2d2872edf1be1c
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-02-09 05:13:23 +0530

    Time: 69 ms (36.80%), Space: 16.4 MB (52.33%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -5,26 +5,27 @@
 #         self.left = left
 #         self.right = right
 
-def calc(node,d):
-    if(node.left and node.right):
-        d[node]=[0,0]
-        d[node][0]=calc(node.left,d)+1
-        d[node][1]=calc(node.right,d)+1
-    elif(node.left):
-        d[node]=[0,0]
-        d[node][0]=calc(node.left,d)+1
-    elif(node.right):
-        d[node]=[0,0]
-        d[node][1]=calc(node.right,d)+1
-    else:
-        return 0
-    return max(d[node][0],d[node][1])
+
+    
     
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        d={}
-        calc(root,d)
+        def calc(node):
+            nonlocal m
+            if(node.right and node.left):
+                le=1+calc(node.left)
+                ri=1+calc(node.right)
+                m=max(m,le+ri)
+                return max(le,ri)
+            elif(node.left):
+                le=1+calc(node.left)
+                m=max(m,le)
+                return le
+            elif(node.right):
+                ri=1+calc(node.right)
+                m=max(m,ri)
+                return ri
+            return 0
         m=0
-        for i in d.values():
-            m=max(i[0]+i[1],m)
+        calc(root)
         return m
