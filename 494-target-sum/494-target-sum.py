d00e0797c2175473cf5493351d6d4fd7f1fc12df
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-02-21 18:47:07 +0530

    Time: 536 ms (30.70%), Space: 111.3 MB (5.46%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,20 +1,19 @@
 class Solution:
         
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        p=[i for i in nums[::-1]]
-        for i in range(1,len(p)):
-            p[i]+=p[i-1]
-        p=p[::-1]
-        
         @cache
         def rec(i,s):
-            if(i==len(nums) and target==s):
-                return 1
-            elif(i==len(nums)):
-                return 0
-            elif(s-p[i]<=target and target<=s+p[i]):
-                return rec(i+1,s-nums[i])+rec(i+1,s+nums[i])
-            else:
-                return 0 
+            if((i,s) not in d):
+                if(i==len(nums) and target==s):
+                    d[(i,s)]=1
+                elif(i==len(nums)):
+                    d[(i,s)]=0
+                else:
+                    d[(i,s)]=rec(i+1,s-nums[i])+rec(i+1,s+nums[i])
+            return d[(i,s)]
         
-        return rec(0,0)
+        d={}
+        d[(0,0)]=rec(0,0)
+        return d[(0,0)]
+        
+        
