50218bf51cab0b2b08d4a79ec8a6cfc79500a47d
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-03-17 22:59:43 +0530

    Time: 2232 ms (65.13%), Space: 30.6 MB (36.25%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,29 +1,21 @@
 from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        ans=[]
-        l=0
-        r=0
-        q=deque([])
-        while(r<k):
-            while(q and nums[q[-1]]<nums[r]):
-                q.pop()
-            q.append(r)
-            r+=1
-        while(r<=len(nums)):
-            # print(q,l)
-            while(q[0]<l):
-                q.popleft()
-            ans.append(nums[q[0]])
-            while(q and r<len(nums) and nums[q[-1]]<nums[r]):
-                q.pop()
-            q.append(r)
-            l+=1
-            r+=1
-        return ans
-            
-                
-        
-            
-            
-        
+        minqueue = deque()
+
+        # initialize the window
+        for i in range(min(k, len(nums))):
+            while minqueue and nums[minqueue[-1]] <= nums[i]: minqueue.pop()
+            minqueue.append(i)
+
+        res = [nums[minqueue[0]]]  # results
+
+        for i in range(k, len(nums)):
+            while minqueue and minqueue[0] <= i - k:  # clean up stale elements
+                minqueue.popleft()
+            while minqueue and nums[minqueue[-1]] <= nums[i]:  # kick out smaller ones
+                minqueue.pop()
+            minqueue.append(i)
+            res.append(nums[minqueue[0]])  # guaranteed to be the max
+
+        return res
