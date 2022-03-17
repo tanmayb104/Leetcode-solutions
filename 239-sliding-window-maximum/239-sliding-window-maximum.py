b30a90a44fdd48c55aa3584c26db081ac084b3ad
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-03-17 22:55:20 +0530

    Time: 2282 ms (62.34%), Space: 30.9 MB (12.58%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,36 +1,29 @@
-from heapq import *
+from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        nums=[-i for i in nums]
         ans=[]
         l=0
         r=0
-        d={}
-        h=[]
-        heapify(h)
+        q=deque([])
         while(r<k):
-            d[nums[r]]=r
-            heappush(h,nums[r])
+            while(q and nums[q[-1]]<nums[r]):
+                q.pop()
+            q.append(r)
             r+=1
-        a=heappop(h)
-        ans.append(-a)
-        if(d[a]!=l):
-            heappush(h,a)
-        while(r<len(nums)):
+        while(r<=len(nums)):
+            # print(q,l)
+            while(q[0]<l):
+                q.popleft()
+            ans.append(nums[q[0]])
+            while(q and r<len(nums) and nums[q[-1]]<nums[r]):
+                q.pop()
+            q.append(r)
             l+=1
-            d[nums[r]]=r
-            heappush(h,nums[r])
-            # print(ans,h,l,r)
-            while(True):
-                a=heappop(h)
-                if(d[a]==l):
-                    break
-                elif(d[a]>l):
-                    heappush(h,a)
-                    break
-            ans.append(-a)
             r+=1
         return ans
+            
+                
+        
             
             
         
