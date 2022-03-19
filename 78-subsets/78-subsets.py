9a961901cefeb8fd6e6860fb404e2e0d7f9ccc27
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-03-19 16:17:44 +0530

    Time: 65 ms (15.19%), Space: 14.1 MB (82.61%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,9 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        l=[[]]
-        for i in nums:
-            j=len(l)
-            while(j):
-                l.append(l[j-1]+[i])
-                j-=1
+        l=[]
+        def solve(nums,i,l,out):
+            if(i==len(nums)):
+                l.append(out)
+                return
+            solve(nums,i+1,l,out)
+            solve(nums,i+1,l,out+[nums[i]])
+        solve(nums,0,l,[])
         return l
