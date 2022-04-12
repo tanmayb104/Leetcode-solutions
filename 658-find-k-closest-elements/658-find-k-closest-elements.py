4f0f3fea235ec608be9dcd9637d7786cbd095ed4
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-04-12 22:53:19 +0530

    Time: 490 ms (27.16%), Space: 15.5 MB (81.73%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,5 +1,20 @@
 from bisect import bisect
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        arr=sorted(arr,key=lambda a:abs(a-x))
-        return sorted(arr[:k])
+        a=bisect_left(arr,x)
+        l=a-1
+        r=a
+        while(k and l>-1 and r<len(arr)):
+            if(abs(arr[l]-x)<=abs(arr[r]-x)):
+                l-=1
+            else:
+                r+=1
+            k-=1
+        if(k):
+            while(k and l>-1):
+                l-=1
+                k-=1
+            while(k and r<len(arr)):
+                r+=1
+                k-=1
+        return arr[l+1:r]
