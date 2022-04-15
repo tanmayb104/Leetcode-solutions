9e9cbeefa9af7909acdbd47f5038876272a30525
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-04-15 23:52:35 +0530

    Time: 539 ms (77.79%), Space: 15.3 MB (53.97%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,11 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # dp=[False for i in range(len(nums))]
-        # dp[0]=True
         m=1
         ma=1
         for i in range(len(nums)):
-            # print(m,ma)
             if(m>i):
                 m=max(m,ma+nums[i])
                 ma+=1
