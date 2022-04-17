e2c821893e66c5a80f395a7cd14d63b89e9bd5ab
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-04-17 22:49:59 +0530

    Time: 765 ms (65.83%), Space: 22.7 MB (27.80%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,6 +1,29 @@
 class Solution:
+    def mergesort(self,nums):
+        if(len(nums)==1):
+            return nums
+        else:
+            mid=len(nums)//2
+            l1=self.mergesort(nums[:mid])
+            l2=self.mergesort(nums[mid:])
+            l=[]
+            i=0
+            j=0
+            while(i<len(l1) and j<len(l2)):
+                if(l1[i]<=l2[j]):
+                    l.append(l1[i])
+                    i+=1
+                else:
+                    l.append(l2[j])
+                    j+=1
+            while(i<len(l1)):
+                l.append(l1[i])
+                i+=1
+            while(j<len(l2)):
+                l.append(l2[j])
+                j+=1
+            return l
+                
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        return nums
-    
+        return self.mergesort(nums)
         
