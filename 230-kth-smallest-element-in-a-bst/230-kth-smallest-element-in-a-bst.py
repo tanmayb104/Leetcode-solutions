4e0ff852cb2a44a944a153d9e39607f25e5ddf12
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-04-18 14:30:25 +0530

    Time: 45 ms (96.55%), Space: 18.1 MB (48.12%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -4,17 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:            
+class Solution:
+    def inorder(self,root):
+        if(root):
+            # print(self.k,root.val)
+            self.inorder(root.left)
+            self.k-=1
+            if(self.k==0):
+                raise Exception(root.val)
+            self.inorder(root.right)
+            
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         self.k=k
-        def inorder(root):
-            if(root):
-                inorder(root.left)
-                self.k-=1
-                if(self.k==0):
-                    raise Exception(root.val)
-                inorder(root.right)
         try:
-            inorder(root)
+            self.inorder(root)
         except Exception as e:
             return e
