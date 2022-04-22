146f3299c05ee8a26749d3f2c246ca7e043b93a0
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-04-22 14:26:22 +0530

    Time: 561 ms (25.22%), Space: 30.3 MB (28.54%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -4,17 +4,46 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def merge(self,head1,head2):
+        # print(head1,head2)
+        head=self.head
+        while(head1 and head2):
+            if(head1.val<=head2.val):
+                head.next=head1
+                head=head.next
+                head1=head1.next
+            else:
+                head.next=head2
+                head=head.next
+                head2=head2.next
+        while(head1):
+            head.next=head1
+            head=head.next
+            head1=head1.next
+        while(head2):
+            head.next=head2
+            head=head.next
+            head2=head2.next
+        # print("111",self.head)
+        return self.head.next
+        
+    def mergesort(self,head):
+        if(not head.next):
+            return head
+        mid=head
+        fast=head
+        while(fast and fast.next):
+            prev=mid
+            mid=mid.next
+            fast=fast.next.next
+        prev.next=None
+        h1=self.mergesort(head)
+        h2=self.mergesort(mid)
+        return self.merge(h1,h2)
+    
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        l=[]
-        a=head
-        while(a):
-            l.append(a.val)
-            a=a.next
-        l.sort()
-        a=head
-        for i in l:
-            a.val=i
-            a=a.next
-        return head
-                    
+        if(not head):
+            return None
+        self.head=ListNode()
+        return self.mergesort(head)            
                     
