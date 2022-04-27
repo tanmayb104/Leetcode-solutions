abe4548f52d212f3e382662bc9c837e90adcdac2
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-04-27 22:30:43 +0530

    Time: 852 ms (60.52%), Space: 64.1 MB (17.52%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,9 +1,11 @@
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         p={}
+        r={}
         for i in range(len(s)):
             p[i]=i
-            
+            r[i]=1
+        print(p)
         def find_set(a):
             if(a==p[a]):
                 return a
@@ -14,7 +16,10 @@ class Solution:
             a=find_set(a)
             b=find_set(b)
             if(a!=b):
+                if(r[a]<r[b]):
+                    a,b=b,a
                 p[b]=a
+                r[a]+=r[b]
         
         for i in pairs:
             union_set(i[0],i[1])
