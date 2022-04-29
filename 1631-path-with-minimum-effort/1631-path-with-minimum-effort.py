b8e7958c07ee4ddc3503af36328398a2d8d59899
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-04-29 16:38:05 +0530

    Time: 749 ms (93.53%), Space: 15.4 MB (77.72%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,31 +1,26 @@
+from heapq import *
 class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
-
-        grid = heights
-        # edge cases:
-        if not grid:
-            return 0
-        
-        from heapq import heappush, heappop
-        h = [(0, (0,0))] # key to heap is cost
-        costSoFar = {(0,0): 0} # also serves as a more nuanced visited set
-        # cameFrom = {(0,0): None} # (child : parent) Only required for path finding (not needed in scope)
-        dirs = [(1,0), (0,1), (-1,0), (0,-1)] # all 4 dirs
-        trgt = (len(grid)-1, len(grid[0])-1) # Dijkstra can have early termination condition
-        import math
-        while h:
-            cost, node = heappop(h)
-            x, y = node
-            if node == trgt: # - Dikstra can have early terminate
-                break
-            for dir in dirs:
-                newX, newY = x+dir[0], y+dir[1]
-                # boundries
-                if newX >= 0 and newX <= len(grid)-1 and newY >= 0 and newY <= len(grid[0])-1:
-                    edgeCost = max(cost, abs(grid[x][y] - grid[newX][newY])) # -- See [Comment I] below
-                    # if nei not seen before or seen before but now revisiting via less costly route
-                    if (newX, newY) not in costSoFar or ( (newX, newY) in costSoFar and edgeCost < costSoFar[(newX, newY)]):
-                        costSoFar[(newX, newY)] = edgeCost
-                        heappush(h, (edgeCost, (newX, newY)))
-                        
-        return costSoFar[trgt]
+    def minimumEffortPath(self, h: List[List[int]]) -> int:
+        n=len(h)
+        m=len(h[0])
+        l=[]
+        heapify(l)
+        heappush(l,tuple([0,0,0]))
+        x=[0,0,1,-1]
+        y=[1,-1,0,0]
+        vis=[[False for i in range(m)] for j in range(n)]
+        while(l):
+            # print(l)
+            a,b,c=heappop(l)
+            if(vis[b][c]):
+                continue
+            if(b==n-1 and c==m-1):
+                return a
+            vis[b][c]=True
+            for i in range(4):
+                sb=b+x[i]
+                sc=c+y[i]
+                if(sb>-1 and sc>-1 and sb<n and sc<m and not vis[sb][sc]):
+                    sa=max(a,abs(h[sb][sc]-h[b][c]))
+                    heappush(l,tuple([sa,sb,sc]))
+        return
