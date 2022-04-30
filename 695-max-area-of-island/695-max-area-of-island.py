215fea13e8e0087d53da60b10db2518ef94c1da4
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-04-30 18:45:46 +0530

    Time: 247 ms (20.36%), Space: 15.2 MB (72.49%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -2,27 +2,42 @@ class Solution:
     def maxAreaOfIsland(self, g: List[List[int]]) -> int:
         n=len(g)
         m=len(g[0])
-        vis=[[False for i in range(m)] for j in range(n)]
-        ans=0
-        x=[1,-1,0,0]
-        y=[0,0,-1,1]
+        x=[1,0]
+        y=[0,1]
+        
+        p=[0 for i in range(n*m)]
+        r=[0 for i in range(n*m)]
         for i in range(n):
             for j in range(m):
-                if(g[i][j]==1 and not vis[i][j]):
-                    q=[[i,j]]
-                    area=0
-                    while(q):
-                        a,b=q.pop()
-                        if(not vis[a][b]):
-                            vis[a][b]=True
-                            area+=1
-                            for k in range(4):
-                                sa=a+x[k]
-                                sb=b+y[k]
-                                if(sa>-1 and sa<n and sb>-1 and sb<m and not vis[sa][sb] and g[sa][sb]==1):
-                                    q.append([sa,sb])
-                    ans=max(ans,area)
-        return ans
+                p[i*m+j]=i*m+j
+                if(g[i][j]==1):
+                    r[i*m+j]=1
+                
+        def find_set(a):
+            if(a==p[a]):
+                return a
+            p[a]=find_set(p[a])
+            return p[a]
+        
+        def union_set(a,b):
+            a=find_set(a)
+            b=find_set(b)
+            if(a!=b):
+                if(r[a]<r[b]):
+                    a,b=b,a
+                p[b]=a
+                r[a]+=r[b]
+        
+        for i in range(n):
+            for j in range(m):
+                if(g[i][j]==1):
+                    for k in range(2):
+                        sa=i+x[k]
+                        sb=j+y[k]
+                        if(sa>-1 and sa<n and sb>-1 and sb<m and g[sa][sb]==1):
+                            union_set(i*m+j,sa*m+sb)
+        
+        return max(r)
                                 
                             
                             
