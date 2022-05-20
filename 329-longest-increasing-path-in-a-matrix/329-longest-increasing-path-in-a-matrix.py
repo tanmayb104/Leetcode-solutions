1e07e8b292028cd9d758e35328ffd1cd2e3cb536
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-05-20 20:23:45 +0530

    Time: 787 ms (23.72%), Space: 21.3 MB (8.01%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,27 +1,26 @@
-from heapq import *
 class Solution:
     def longestIncreasingPath(self, ma: List[List[int]]) -> int:
         x=[0,0,1,-1]
         y=[1,-1,0,0]
-        h=[]
-        heapify(h)
         n=len(ma)
         m=len(ma[0])
-        for i in range(n):
-            for j in range(m):
-                heappush(h,[-ma[i][j],i,j])
-        l=[[0 for i in range(m)] for j in range(n)]
-        maxim=0
-        while(h):
-            a,b,c=heappop(h)
-            maxi=0
+        
+        def rec(b,c):
+            l=0
+            if((b,c) in d):
+                return d[(b,c)]
             for i in range(4):
                 sb=b+x[i]
                 sc=c+y[i]
                 if(0<=sb<n and 0<=sc<m and ma[b][c]<ma[sb][sc]):
-                    maxi=max(maxi,l[sb][sc])
-            l[b][c]=maxi+1
-            maxim=max(maxim,l[b][c])
-        # print(l)
-        return maxim
-                    
+                    l=max(l,rec(sb,sc))
+            d[(b,c)]=1+l
+            return 1+l
+        
+        maxi=0
+        d={}
+        for i in range(n):
+            for j in range(m):
+                maxi=max(maxi,rec(i,j))
+        # print(d)
+        return maxi
