1090d7a4e1e5a5d547f4f70913806166e2d389ef
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-05-20 20:24:55 +0530

    Time: 648 ms (44.58%), Space: 20.8 MB (9.38%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -5,22 +5,18 @@ class Solution:
         n=len(ma)
         m=len(ma[0])
         
+        @lru_cache(maxsize=None)
         def rec(b,c):
             l=0
-            if((b,c) in d):
-                return d[(b,c)]
             for i in range(4):
                 sb=b+x[i]
                 sc=c+y[i]
                 if(0<=sb<n and 0<=sc<m and ma[b][c]<ma[sb][sc]):
                     l=max(l,rec(sb,sc))
-            d[(b,c)]=1+l
             return 1+l
         
         maxi=0
-        d={}
         for i in range(n):
             for j in range(m):
                 maxi=max(maxi,rec(i,j))
-        # print(d)
         return maxi
