f7cc4736eba0363631f5f7a9620b32671498b5d9
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-05-21 14:25:34 +0530

    Time: 2345 ms (29.05%), Space: 14.2 MB (46.46%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,17 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @lru_cache(maxsize=None)
-        def rec(i,a):
-            if(a<0):
-                return 999999999999
-            if(a==0):
-                return 0
-            if(i==len(coins)-1):
-                return 1+rec(i,a-coins[i])
-            return min(1+rec(i,a-coins[i]),rec(i+1,a))
-        
-        a=rec(0,amount)
-        if(a>=999999999999):
-            return -1
-        return a
+        dp=[-1 for i in range(amount+1)]
+        dp[0]=0
+        for i in range(len(coins)):
+            if(coins[i]<amount+1):
+                dp[coins[i]]=1
+        for i in range(amount+1):
+            if(dp[i]!=-1):
+                for j in range(len(coins)):
+                    a=i+coins[j]
+                    if(a<amount+1):
+                        if(dp[a]!=-1):
+                            dp[a]=min(dp[a],dp[i]+1)
+                        else:
+                            dp[a]=dp[i]+1
+        return dp[amount]
                 
