aa4c7249be6d7b20315ece79afe269fd52a33fa2
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-06-06 19:15:07 +0530

    Time: 105 ms (33.93%), Space: 13.9 MB (80.46%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -0,0 +1,48 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        board=[["." for i in range(n)] for j in range(n)]
+        vis=[[0 for i in range(n)] for j in range(n)]
+        self.ans=0
+        
+        def rec(i,l):
+            # print(board)
+            # print(vis)
+            # print(i,l)
+            # print("****88",self.ans)
+            if(i==n):
+                p=[]
+                for i in range(n):
+                    o=""
+                    for j in board[i]:
+                        o+=j
+                    p.append(o)
+                self.ans+=1
+                return
+            else:
+                for j in range(n):
+                    if(vis[i][j]==0):
+                        board[i][j]="Q"
+                        attack=[]
+                        a=i+1
+                        b=j+1
+                        c=j-1
+                        while(a<n):
+                            if(b<n):
+                                attack.append([a,b])
+                                vis[a][b]+=1
+                                b+=1
+                            attack.append([a,j])
+                            vis[a][j]+=1
+                            if(c>-1):
+                                attack.append([a,c])
+                                vis[a][c]+=1
+                                c-=1
+                            a+=1
+                        l.append(attack)
+                        rec(i+1,l)
+                        at=l.pop()
+                        for k in at:
+                            vis[k[0]][k[1]]-=1
+                        board[i][j]="."
+        rec(0,[])
+        return self.ans
