80fc5a7c931ce8e363b41ed5b8b42dbd21d76e8d
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-06-14 20:31:46 +0530

    Time: 732 ms (5.60%), Space: 16 MB (81.15%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,7 +1,14 @@
 class Solution:
     def minDistance(self, w1: str, w2: str) -> int:
-        dp = [[1000]*(len(w2)+1) for i in range(2)]
-        for i in range(len(w1) + 1):
-            for j in range(len(w2) + 1):
-                dp[i & 1][j] = i + j if i == 0 or j == 0 else dp[(i - 1) & 1][j - 1] if w1[i - 1] == w2[j - 1] else 1 + min(dp[(i - 1) & 1][j], dp[i & 1][j - 1])
-        return dp[len(w1) & 1][-1]
+        
+        L1, L2 = len(w1), len(w2)
+        @lru_cache()
+        def solve(i, j):
+            if i == L1 and j == L2 : return 0
+            if i == L1 or j == L2 : 
+                return max(L1 - i, L2 - j)
+            if w1[i] == w2[j] : 
+                return solve(i + 1, j + 1)                
+            return 1 + min(solve(i + 1, j), solve(i, j + 1))    
+        
+        return solve(0, 0) 
