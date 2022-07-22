bcabfb3d50ff9e9f705c12ac339a7a4311a3529c
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-07-23 04:00:18 +0530

    Time: 4335 ms (5.00%), Space: 466.8 MB (5.50%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,6 +1,6 @@
 class Solution:
     def coinChange(self, c: List[int], a: int) -> int:
-        c.sort(reverse=True)
+        c.sort()
         d={}
         def rec(i,a):
             tu=tuple([i,a])
@@ -13,8 +13,7 @@ class Solution:
             if(c[i]<=a):
                 d[tu]=min(1+rec(i,a-c[i]),rec(i+1,a))
                 return d[tu]
-            d[tu]=rec(i+1,a)
-            return d[tu]
+            return 9999999999999999
         
         ans=rec(0,a)
         if(ans>=9999999999999999):
