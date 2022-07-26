3551747aa3259f86814d7f6b30f86bf5998b8841
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-07-26 18:43:02 +0530

    Time: 77 ms (88.15%), Space: 26.1 MB (82.46%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,34 +8,17 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        l1=[]
-        l2=[]
-        def dfs(root,t):
-            nonlocal l1
-            if(root==t):
-                l1.append(root)
-                raise Exception(l1)
-            if(root.left):
-                l1.append(root)
-                dfs(root.left,t)
-                l1.pop()
-            if(root.right):
-                l1.append(root)
-                dfs(root.right,t)
-                l1.pop()
-        try:
-            dfs(root,p)
-        except Exception as e:
-            l2=[i for i in l1]
-            l1=[]
-        try:
-            dfs(root,q)
-        except:
-            pass
-        prev=-1
-        # print(l1,l2)
-        for i in range(min(len(l1),len(l2))):
-            if(l1[i]!=l2[i]):
-                return prev
-            prev=l1[i]
-        return prev
+        def rec(root):
+            if(not root):
+                return False
+            l=rec(root.left)
+            r=rec(root.right)
+            if(l and r):
+                return root
+            elif((root==p or root==q) and (l or r)):
+                return root
+            elif(root==p or root==q):
+                return True
+            else:
+                return l or r
+        return rec(root)
