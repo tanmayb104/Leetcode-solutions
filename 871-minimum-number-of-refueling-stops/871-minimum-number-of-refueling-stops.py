124f35d16ab9689e31467ec4237045db47aeb6aa
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-08-22 21:25:34 +0530

    Time: 198 ms (59.30%), Space: 14.3 MB (36.38%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
@@ -1,16 +1,27 @@
+from heapq import *
 class Solution:
-    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        stations.append([target, 0])    
-        fuel = startFuel
-        cnt, prev = 0, 0
-        miss = []
-        for pos, gas in stations:
-            dis, prev = pos - prev, pos    
-            if fuel < dis:  
-                while miss and fuel < dis:  
-                    fuel += -heapq.heappop(miss)
-                    cnt += 1    
-                if fuel < dis: return -1    
-            fuel -= dis
-            heapq.heappush(miss, -gas)  
-        return cnt
+    def minRefuelStops(self, t: int, s: int, st: List[List[int]]) -> int:
+    
+        h=[]
+        st+=[[t,0]]
+        heapify(h)
+        pos=0
+        ans=0
+        i=0
+        while(i<len(st)):
+            # print(pos,s,h)
+            if(s>=st[i][0]-pos):
+                s-=st[i][0]-pos
+                heappush(h,-st[i][1])
+                pos=st[i][0]
+                i+=1
+            elif(h):
+                while(h and s<st[i][0]-pos):
+                    ans+=1
+                    a=heappop(h)
+                    s-=a
+            else:
+                break
+        if(i==len(st)):
+            return ans
+        return -1
