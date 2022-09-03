9fe99c5090386b00cebae88b00faf4695555896b
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-09-03 22:22:03 +0530

    Time: 42 ms (94.56%), Space: 14.4 MB (12.93%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,7 +1,6 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
         ans=[]
-        @lru_cache()
         def rec(n,k,i,s):
             if(i==n):
                 ans.append(s)
@@ -9,7 +8,7 @@ class Solution:
             else:
                 if(int(s[-1])+k<10):
                     rec(n,k,i+1,s+str(int(s[-1])+k))
-                if(int(s[-1])-k>-1):
+                if(int(s[-1])-k>-1 and k!=0):
                     rec(n,k,i+1,s+str(int(s[-1])-k))
         for i in range(1,10):
             rec(n,k,1,str(i))
