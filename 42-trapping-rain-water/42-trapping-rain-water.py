a953f54f8fe6f6df01044409f9a5159657933d67
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-09-18 11:58:24 +0530

    Time: 296 ms (17.03%), Space: 16 MB (81.24%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, bars):
+        if not bars or len(bars) < 3:
+            return 0
+        volume = 0
+        left, right = 0, len(bars) - 1
+        l_max, r_max = bars[left], bars[right]
+        while left < right:
+            l_max, r_max = max(bars[left], l_max), max(bars[right], r_max)
+            if l_max <= r_max:
+                volume += l_max - bars[left]
+                left += 1
+            else:
+                volume += r_max - bars[right]
+                right -= 1
+        return volume
+        
