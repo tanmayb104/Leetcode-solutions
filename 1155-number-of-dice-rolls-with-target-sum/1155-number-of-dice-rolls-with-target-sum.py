a3b6cd11d8fbecebd30a953070117138b7a4de51
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-10-02 16:25:59 +0530

    Time: 112 ms (97.47%), Space: 15.2 MB (57.48%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, t: int) -> int:
+        @lru_cache(maxsize=None)
+        def rec(i,t):
+            if(i==n and t==0):
+                return 1
+            elif(t<n-i):
+                return 0
+            elif((n-i)*k<t):
+                return 0
+            elif(i==n):
+                return 0
+            elif(t<0):
+                return 0
+            else:
+                ans=0
+                for j in range(1,k+1):
+                    ans+=rec(i+1,t-j)
+                return ans%(10**9+7)
+        return rec(0,t)%(10**9+7)
+        
