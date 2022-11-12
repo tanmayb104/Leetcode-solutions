bbae3d18e240421471a396882a075bce628853af
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2022-11-12 14:03:24 +0530

    Time: 1504 ms (30.09%), Space: 35.7 MB (80.22%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,19 @@
+from heapq import *
+
+
+class MedianFinder:
+    def __init__(self):
+        self.small = []
+        self.large = []  
+
+    def addNum(self, num):
+        if len(self.small) == len(self.large):
+            heappush(self.large, -heappushpop(self.small, -num))
+        else:
+            heappush(self.small, -heappushpop(self.large, num))
+
+    def findMedian(self):
+        if len(self.small) == len(self.large):
+            return float(self.large[0] - self.small[0]) / 2.0
+        else:
+            return float(self.large[0])
