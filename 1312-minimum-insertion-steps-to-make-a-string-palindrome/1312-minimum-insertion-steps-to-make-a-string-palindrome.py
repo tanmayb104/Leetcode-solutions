0eac875638fc3584f51b5b960d66a294ddc90079
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2023-04-22 16:26:16 +0530

    Time: 855 ms (65.72%), Space: 167.1 MB (14.61%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,17 +1,12 @@
 class Solution:
-    def minInsertions(self, s1: str) -> int:
-        
-        d={}
-        s2=s1[::-1]
-        def rec(i1,i2):
-            if(i1==len(s1) or i2==len(s2)):
-                return 0
-            tu=tuple([i1,i2])
-            if(tu in d):
-                return d[tu]
-            if(s1[i1]==s2[i2]):
-                d[tu]=rec(i1+1,i2+1)+1
+    def minInsertions(self, s: str) -> int:
+        @cache
+        def rec(l,r):
+            if(l<r):
+                if(s[l]==s[r]):
+                    return rec(l+1,r-1)
+                else:
+                    return min(1+rec(l,r-1),1+rec(l+1,r))
             else:
-                d[tu]=max(rec(i1+1,i2),rec(i1,i2+1))
-            return d[tu]
-        return len(s1)-rec(0,0)
+                return 0
+        return rec(0,len(s)-1)
