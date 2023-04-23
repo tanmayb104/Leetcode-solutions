e15d38fc1244da55da22a4edaa56d0e5386444d0
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2023-04-23 15:11:48 +0530

    Time: 1254 ms (95.89%), Space: 17.8 MB (95.89%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfArrays(self, s: str, k: int) -> int:
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[-1] = 1
+        
+        for i in range(n - 1, -1, -1):
+            if s[i] == '0':
+                continue
+            num = 0
+            j = i
+            while j < n and int(s[i:j+1]) <= k:
+                num += dp[j+1]
+                j += 1
+            dp[i] = num % (10 ** 9 + 7)
+        
+        return dp[0]
