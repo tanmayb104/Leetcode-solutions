883346e0e0cf6e5859260b01534d7e34e7623950
Tanmay Bansal <59735148+tanmayb104@users.noreply.github.com>
2023-05-31 15:21:45 +0530

    Time: 260 ms (24.29%), Space: 26.5 MB (35.02%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,33 +1,21 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.p={}
-        self.g={}
+        self.journey = {}
+        self.history = {} # (startStation, endStation) => (allTime, allCount)
+
+    def checkIn(self, id: int, startStation: str, t: int) -> None:
+        self.journey[id] = (startStation, t)
         
 
-    def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.p[id]=[t,stationName]
-
-    def checkOut(self, id: int, stationName: str, t: int) -> None:
-        a,b=self.p[id]
-        time=t-a
-        if(b in self.g and stationName in self.g[b]):
-            self.g[b][stationName].append(time)
-        elif(b in self.g):
-            self.g[b][stationName]=[time]
-        else:
-            self.g[b]={}
-            self.g[b][stationName]=[time]
+    def checkOut(self, id: int, endStation: str, endTime: int) -> None:
+        startStation, startTime = self.journey.pop(id)
+        key = (startStation, endStation)
+        allTime, allCount = self.history.get(key, (0, 0))
+        self.history[key] = (allTime + (endTime - startTime), allCount + 1)
         
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        # print(self.g,startStation,endStation)
-        return sum(self.g[startStation][endStation])/len(self.g[startStation][endStation])
-        
-
-
-# Your UndergroundSystem object will be instantiated and called as such:
-# obj = UndergroundSystem()
-# obj.checkIn(id,stationName,t)
-# obj.checkOut(id,stationName,t)
-# param_3 = obj.getAverageTime(startStation,endStation)
+        key = (startStation, endStation)
+        allTime, allCount = self.history.get(key, (0, 0))
+        return allTime / allCount
